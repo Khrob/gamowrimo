9d8e74d0ee2ec42df24af6a89725412d5e8d9577
Khrob Edmonds <khedmonds@gmail.com>
2019-11-01 08:47:57 -0700

    Initial Commit

---
+++ gamo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  gamo
+//
+//  Created by Khrob Edmonds on 11/1/19.
+//  Copyright © 2019 TangoSoup. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ gamo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  gamo
+//
+//  Created by Khrob Edmonds on 11/1/19.
+//  Copyright © 2019 TangoSoup. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
